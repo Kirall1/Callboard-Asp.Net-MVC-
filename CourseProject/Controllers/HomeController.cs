f03781e6b6251a3b7f89d400e32a61bab40b3441
Kir <kirill.grudinski@gmail.com>
2023-12-03 15:48:32 +0300

    add ads view

--- CourseProject/Controllers/HomeController.cs
+++ CourseProject/Controllers/HomeController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CourseProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CourseProject.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private IPlatformRepository repository;
+
+        public HomeController(IPlatformRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index() => View(repository.Ads);
     }
 
 }
