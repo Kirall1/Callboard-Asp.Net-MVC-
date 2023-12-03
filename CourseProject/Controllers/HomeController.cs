c103013a48d28008b8e2ffe2b7f02cb931f24dba
Kir <kirill.grudinski@gmail.com>
2023-12-03 15:31:39 +0300

    Creating DbContext, migrations and seed data

---
+++ CourseProject/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+
+}
